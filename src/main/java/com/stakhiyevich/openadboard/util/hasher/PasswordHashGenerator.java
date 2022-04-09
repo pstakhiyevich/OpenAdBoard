0b9af827f9a570b632f074b1396763d25204e0dd
Pavel <t329023@gmail.com>
2022-04-09 16:09:27 +0300

    implement PasswordHashGenerator

---
+++ src/main/java/com/stakhiyevich/openadboard/util/hasher/PasswordHashGenerator.java
@@ -0,0 +1,25 @@
+package com.stakhiyevich.openadboard.util.hasher;
+
+import java.util.Optional;
+
+/**
+ * A Password hash generator interface that generates and checks a password's hash.
+ */
+public interface PasswordHashGenerator {
+    /**
+     * Generates a hash from a specified user password.
+     *
+     * @param password a user's password
+     * @return an optional object of a generated password hash
+     */
+    Optional<String> generatePasswordHash(String password);
+
+    /**
+     * Checks whether a password matches a hash.
+     *
+     * @param password a user's password
+     * @param key a user password's hash
+     * @return whether a password matches a hash
+     */
+    boolean checkPasswordHash(String password, String key);
+}
---
+++ src/main/java/com/stakhiyevich/openadboard/util/hasher/impl/PasswordHashGeneratorImpl.java
@@ -0,0 +1,65 @@
+package com.stakhiyevich.openadboard.util.hasher.impl;
+
+import com.stakhiyevich.openadboard.util.hasher.PasswordHashGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Optional;
+
+public class PasswordHashGeneratorImpl implements PasswordHashGenerator {
+    private static final Logger logger = LogManager.getLogger();
+    private static PasswordHashGeneratorImpl instance;
+
+    private static final SecureRandom RAND = new SecureRandom();
+    private static final int ITERATIONS = 65536;
+    private static final int KEY_LENGTH = 512;
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
+    private static final String SALT = "sChHR5DLE1hb";
+
+    private PasswordHashGeneratorImpl() {
+    }
+
+    public static PasswordHashGeneratorImpl getInstance() {
+        if (instance == null) {
+            instance = new PasswordHashGeneratorImpl();
+        }
+        return instance;
+    }
+
+    @Override
+    public Optional<String> generatePasswordHash(String password) {
+        char[] chars = password.toCharArray();
+        byte[] bytes = SALT.getBytes();
+        PBEKeySpec spec = new PBEKeySpec(chars, bytes, ITERATIONS, KEY_LENGTH);
+        Arrays.fill(chars, Character.MIN_VALUE);
+        try {
+            SecretKeyFactory fac = SecretKeyFactory.getInstance(ALGORITHM);
+            byte[] securePassword = fac.generateSecret(spec).getEncoded();
+            return Optional.of(Base64.getEncoder().encodeToString(securePassword));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            logger.error("can't generate password hash", e);
+            return Optional.empty();
+        } finally {
+            spec.clearPassword();
+        }
+    }
+
+    @Override
+    public boolean checkPasswordHash(String password, String key) {
+        Optional<String> optEncrypted = generatePasswordHash(password);
+        return optEncrypted.map(s -> s.equals(key)).orElse(false);
+    }
+
+    private String generateSalt() {
+        byte[] salt = new byte[KEY_LENGTH];
+        RAND.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+}
